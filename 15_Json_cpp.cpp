0d24a5ac6d1f77f4caaa3aeb8279974f3637e884
xuejun <xuejunzhao0@gmail.com>
2023-05-26 15:34:15 +0800

    update jsoncpp

--- 15_Json_cpp.cpp
+++ 15_Json_cpp.cpp
@@ -3,6 +3,7 @@
 //
 /*开始本文件的测试之前，确定自己的系统安装了jsoncpp库，github地址如下 https://github.com/open-source-parsers/jsoncpp
  *Json文件是C++面向对象编程更常见的文件格式，借助json开源库能够快速的实现json文件的解析
+ * Json主要有两种数据格式，分别为Json数组和Json对象
  */
 #include <json/json.h>
 #include <fstream>
@@ -15,14 +16,19 @@ void WriteJson()
     Value root;
     root.append("Naruto");
     root.append("Village of Fire");
+
     Value dict1;
     dict1["Sex"]="man";
     dict1["Class"]="Level Master";
     Value dict2;
     dict2["Ability"]="Wind";
     dict2["Tasks"]="Level S";
+    Value dict3;
+    dict3["Magic"]= "Multi-Person";
+    dict3["Body"]= "Bullet-proof";
+    dict3["Field"] = dict2;
     root.append(dict1);
-    root.append(dict2);
+    root.append(dict3);
     string str = root.toStyledString();
     ofstream of("Naruto.json");
     of<<str;
